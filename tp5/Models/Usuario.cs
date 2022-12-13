13d8a1cb185f9cb3e16806f96b9f2e209b1cc1ef
Octavio Llanillo <llanillo4@hotmail.com>
2022-12-12 22:30:03 -0300

    Create Usuario.cs

---
+++ tp5/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿namespace tp5.Models;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    
+    public string Nombre { get; set; }
+    
+    public string NombreUsuario { get; set; }
+    
+    public string Contrasena { get; set; }
+    
+    public string Rol { get; set; }
+
+    public Usuario()
+    {
+        
+    }
+    
+    public Usuario(int id, string nombre, string nombreUsuario, string contrasena, string rol)
+    {
+        Id = id;
+        Nombre = nombre;
+        NombreUsuario = nombreUsuario;
+        Contrasena = contrasena;
+        Rol = rol;
+    }
+}
